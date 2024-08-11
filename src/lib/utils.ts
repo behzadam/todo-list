c54b870522b3cb150ee301741f23199ef08c7ade
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2024-08-11 13:59:55 +0330

    add comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,24 +2,23 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { v4 as uuidv4 } from "uuid";
 
+/**
+ * Shadcn util function that merges classes
+ * @param inputs
+ * @returns
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(dateString: string) {
-  const date = new Date(dateString);
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZoneName: "short",
-  };
-  return date.toLocaleString("en-US", options);
-}
-
+/**
+ * Format a date string to a short date format.
+ * We can also use next-intl:
+ * {@link} https://next-intl-docs.vercel.app/docs/usage/dates-times
+ *
+ * @param dateString the given full date
+ * @returns for example: Aug 11, 2024
+ */
 export function formatDateShort(dateString: string) {
   const date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = {
@@ -30,6 +29,10 @@ export function formatDateShort(dateString: string) {
   return date.toLocaleString("en-US", options);
 }
 
+/**
+ *  Generates UUID
+ * @returns for example: 141404e5-f231-4576-9e6f-7aba8b2c0159
+ */
 export function generateUUID() {
   return uuidv4();
 }
